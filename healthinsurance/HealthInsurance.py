e4329c41ef104cb6d540f1361cff37f0078ce866
jefferson-datascience <jefferson_datascience@outlook.com>
2022-12-25 10:48:09 -0300

    'final_fix'

--- healthinsurance/HealthInsurance.py
+++ healthinsurance/HealthInsurance.py
@@ -95,7 +95,7 @@ class HealthInsurance(object):
         dataframe_original = dataframe_original.sort_values(by=['propensity_score'], ascending=False).reset_index(drop=True)
 
         # colunas importantes
-         dataframe_original = dataframe_original[['id', 'propensity_score']]
+        dataframe_original = dataframe_original[['id', 'propensity_score']]
 
         # Criar a coluna ranking
         dataframe_original['ranking'] = dataframe_original.index + 1
